cdd6af82e6598022b6bc98d67106aab99ebbd1b0
avanscoyoc <amy.e.van.scoyoc@gmail.com>
2025-02-26 10:43:49 -0800

    added cloud data and unzip to bash script; updates to dir path in notebook

--- .devcontainer/scripts/post_create/download_input_data.sh
+++ .devcontainer/scripts/post_create/download_input_data.sh
@@ -1,13 +1,22 @@
 #/bin/bash
 
-# Check if input data directory exists
-# if [ ! -d "/workspaces/bird_new/data" ]; then
+Check if input data directory exists
 
-#     # Create input data directory
-#     mkdir -p /workspaces/bird_new/data
+if [ ! -d "/workspaces/bird_new/data" ]; then
 
-#     # Download the data, shared as public links from Google Cloud Storage Bucket
-#     echo "Downloading input data..."
-#     wget -O /workspaces/bird_new/data/audio https://console.cloud.google.com/storage/browser/dse-staff/audio    #FIXME: Add path to google cloud storage bucket
-   
-# fi
+    # Create input data directory
+
+    mkdir -p /workspaces/bird_new/data
+
+    # Download the data, shared as public links from Google Cloud Storage Bucket
+
+    echo "Downloading input data..."
+
+    wget -O /workspaces/bird_new/data/non-avian_ML.zip https://storage.cloud.google.com/dse-staff/audio/non-avian_ML.zip   
+    
+    # Unzip the downloaded file
+    unzip /workspaces/bird_new/data/non-avian_ML.zip -d /workspaces/bird_new/data
+
+    # Remove the zip file after extraction
+    rm /workspaces/bird_new/data/non-avian_ML.zip
+fi
